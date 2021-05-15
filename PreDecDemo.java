fecae63cfb9c8e17bf190e41e78481e54e166ee4
girijapj03 <72901278+girijapj03@users.noreply.github.com>
2021-05-15 19:35:47 +0530

    Add files via upload
    basic operators used and observed how they work

---
+++ PreDecDemo.java
@@ -0,0 +1,12 @@
+class UnaryPostDecDemo{
+public static void main(String[] args){
+int p=50;
+int q=p--;
+System.out.println(p--);//49
+System.out.println(q);//50
+System.out.println(q--);
+System.out.println(q);
+
+}
+}
+
---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,10 @@
+class TernaryOperatorDemo{
+public static void main(String[] args){
+int x=100;
+int y=500;
+int result=x>y?x:y;
+System.out.println("result:" +result);
+
+}
+}
+
